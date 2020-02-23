9305dec5b380f2cb2f7c7e758b53bb19e5b573f8
mrdear <niudear@foxmail.com>
2020-02-23 20:32:56 +0800

    1. finish develop

---
+++ src/main/java/cn/mrdear/intellij/related/RelatedFileLineMarkerProvider.java
@@ -0,0 +1,71 @@
+package cn.mrdear.intellij.related;
+
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
+import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
+import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiDocCommentOwner;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiIdentifier;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.javadoc.PsiDocComment;
+import com.intellij.psi.javadoc.PsiDocTag;
+import com.intellij.psi.javadoc.PsiDocTagValue;
+import com.intellij.util.PlatformIcons;
+
+import org.jetbrains.annotations.NotNull;
+
+import cn.mrdear.intellij.related.util.FileUtils;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * 定位注释中的 @doc ,定位到指定文件
+ * @doc example.gif
+ * @author Quding Ding
+ * @since 2020/2/23
+ */
+public class RelatedFileLineMarkerProvider extends RelatedItemLineMarkerProvider {
+
+    @Override
+    protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo> result) {
+        PsiElement parent = element.getParent();
+        if (element instanceof PsiIdentifier && (parent instanceof PsiMethod || parent instanceof PsiClass)) {
+            // 定位到注释
+            PsiDocComment docComment = ((PsiDocCommentOwner) parent).getDocComment();
+            if (null == docComment) {
+                return;
+            }
+            // 定位到tags
+            PsiDocTag[] docs = docComment.findTagsByName("doc");
+            if (docs.length == 0) {
+                return;
+            }
+            for (PsiDocTag doc : docs) {
+                // 找到值
+                PsiDocTagValue value = doc.getValueElement();
+                if (null == value) {
+                    continue;
+                }
+                String file = value.getText();
+                if (!file.contains(".")) {
+                    continue;
+                }
+
+                List<PsiElement> files = FileUtils.queryFile(element.getProject(), file);
+                if (files.isEmpty()) {
+                    continue;
+                }
+                NavigationGutterIconBuilder<PsiElement> builder =
+                    NavigationGutterIconBuilder.create(PlatformIcons.CUSTOM_FILE_ICON)
+                        .setTargets(files)
+                        .setTooltipText("Navigate to this files");
+                result.add(builder.createLineMarkerInfo(doc.getFirstChild()));
+            }
+        }
+
+
+    }
+
+}
