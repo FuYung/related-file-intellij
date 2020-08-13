8e44f5bf6b879dcc3ed16146e2c768ebb5b7cca1
mrdear <niudear@foxmail.com>
2020-08-13 22:43:09 +0800

    1. support 2020.2 2. 修改判断方式

--- src/main/java/cn/mrdear/intellij/related/RelatedFileLineMarkerProvider.java
+++ src/main/java/cn/mrdear/intellij/related/RelatedFileLineMarkerProvider.java
@@ -3,11 +3,7 @@ package cn.mrdear.intellij.related;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo;
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider;
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiDocCommentOwner;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiIdentifier;
-import com.intellij.psi.PsiMethod;
 import com.intellij.psi.javadoc.PsiDocComment;
 import com.intellij.psi.javadoc.PsiDocTag;
 import com.intellij.psi.javadoc.PsiDocTagValue;
@@ -29,12 +25,9 @@ public class RelatedFileLineMarkerProvider extends RelatedItemLineMarkerProvider
     protected void collectNavigationMarkers(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo<?>> result) {
 
         PsiElement parent = element.getParent();
-        if (element instanceof PsiIdentifier && (parent instanceof PsiMethod || parent instanceof PsiClass)) {
+        if (element instanceof PsiDocTag && (parent instanceof PsiDocComment)) {
             // 定位到注释
-            PsiDocComment docComment = ((PsiDocCommentOwner) parent).getDocComment();
-            if (null == docComment) {
-                return;
-            }
+            PsiDocComment docComment = (PsiDocComment)parent;
             // 定位到tags
             PsiDocTag[] docs = docComment.findTagsByName("doc");
             if (docs.length == 0) {
